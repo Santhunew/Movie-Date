cb5fe9f08b09cfc008d2986e8ee44f505ad2e8a9
santhosh <santhosh.ss7878@gmail.com>
2025-01-12 20:42:24 +0530

    modified

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-12345678" # Replace with a valid AMI ID
+  ami           = "ami-09b0a86a2c84101e1" # Replace with a valid AMI ID
   instance_type = "t2.micro"
 
   tags = {
