37b262f0a0ae24f902af8608070710abb934b7cd
santhosh <santhosh.ss7878@gmail.com>
2025-01-12 23:02:21 +0530

    webapp

--- main.tf
+++ main.tf
@@ -1,12 +1,63 @@
-provider "aws" {
-  region = "us-east-1" # Replace with your AWS region
+resource "aws_security_group" "webappsg" {
+  name = "Mine_webappsg"
+  description = "Allow HTTP and SSH inbound traffic"
+
+ingress {
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+  from_port = 8080
+  to_port = 8080
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+  from_port = 8081
+  to_port = 8081
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+  from_port = 9000
+  to_port = 9000
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+egress {
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-09b0a86a2c84101e1" # Replace with a valid AMI ID
-  instance_type = "t2.micro"
-
+resource "aws_instance" "webapp" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  key_name = var.key_name
+  security_groups = [aws_security_group.webappsg.name]
   tags = {
-    Name = "GitHubActions-Terraform"
+    Name = "webapp_tg"
   }
 }
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+  description = "value of the AMI ID"
+  default = "ami-09b0a86a2c84101e1"
+}
+
+variable "instance_type" {
+  description = "value of the instance type"
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "value of the key name"
+  default = "Santhu"
+}
+
+variable "region" {
+  description = "value of the region"
+  default = "ap-sounth-1"
+}
