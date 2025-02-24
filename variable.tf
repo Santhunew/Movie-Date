d4aa15e9c29d502379a54431dd2e5cb61eb698a5
santhosh <santhosh.ss7878@gmail.com>
2025-02-24 20:13:14 +0530

    updated variables file

--- variable.tf
+++ variable.tf
@@ -30,7 +30,7 @@ variable "ecs_task_execution_role_name" {
 
 variable "ecs_cluster_name" {
   description = "The name of the ECS cluster."
-  default     = "my_cluster"
+  default     = "my_clustersanthu"
   type = string
 }
 
